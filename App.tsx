c4fb545f49203af0a9c496758c4031094b91965d
rahulrao6610 <22bk1a6610@stpetershyd.com>
2024-12-19 15:31:41 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './Components/Home'
+import Movies from './Components/Movies'
+
+export default function App() {
+  return (
+    <div>
+      <Home/>
+      <Movies/>
+    </div>
+  )
+}
